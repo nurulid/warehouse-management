0d2a3a5effa17830f6a7b2aa1e3fbd29320b6e5e
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-05 21:27:32 +0700

    feat: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary : "#6E3FF3",
+        secondary: {
+          75: "#F5F4F7",
+          500: "#908C95"
+        },
+        info: "#3F6FF2",
+        success: "#4EB77B",
+        warning: "#EFD01D",
+        danger: "#F31B09",
+      }
     },
   },
   plugins: [],

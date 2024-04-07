e7d93119fd55fb4a5c56f5ddf6061a50f3a1e52a
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-07 20:13:43 +0700

    feat: improve dashboard heading

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Search } from "./components/Search";
 import { Card } from "./components/Card";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -10,9 +11,14 @@ export default function Page() {
       </div>
       <div className="h-[calc(100vh-65px)] overflow-scroll">
         <div className="p-6">
-          <div className="mb-5">
-            <h2 className="text-2xl font-semibold">Hello, Ernst</h2>
-            <p className="text-gray-400">Warehouse Manager</p>
+          <div className="mb-4 inline-flex gap-2 items-center">
+            <figure className="bg-white border border-secondary-200 rounded p-2">
+              <Image src="/icons/building-2-line.svg" height={24} width={24} alt="icon"/>
+            </figure>
+            <div>
+              <h2 className="text-xl font-[500] leading-6">Hello, Ernst</h2>
+              <p className="text-ink-100 text-xs">Warehouse Manager</p>
+            </div>
           </div>
           <div className="grid grid-cols-3 gap-5 h-full">
             <Card 
@@ -38,16 +44,16 @@ export default function Page() {
             <Card 
               title="Vehicle Tracking"
               icon="/icons/focus-3-line.svg"
-              pageUrl="/products"
+              pageUrl="/vehicles"
               pageTitle="View Tracking"
             > 
               <div>
-                CHART
+                <h3 className="uppercase text-ink-100 text-xs">Vehicles in transit</h3>
               </div>
             </Card>
             <div className="col-span-3">
               <h3 className="mt-2 mb-5 text-xl">Recent Orders</h3>
-              <div className="border rounded-xl p-5 h-[700px] overflow-scroll"></div>
+              <div className="bg-white border rounded-xl p-5 h-[700px] overflow-scroll"></div>
             </div>
           </div>
         </div>

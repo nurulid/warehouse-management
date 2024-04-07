8490ed49b0dff7e1d8cba50dcc9ab4ea0a265cb1
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-08 00:41:57 +0700

    feat: add link component for card

--- app/(dashboard)/dashboard/components/Card.tsx
+++ app/(dashboard)/dashboard/components/Card.tsx
@@ -1,23 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { PropsWithChildren } from "react";
+import { LinkCard } from "./LinkCard";
 
 export const Card = (
   props: PropsWithChildren<{ title: string; icon: string; pageUrl: string, pageTitle: string }>
 ) => {
   const { title, icon, pageUrl, pageTitle, children } = props;
   return (
-    <div className="bg-white border shadow-sm rounded-xl p-4 h-[300px]">
+    <div className="bg-white border shadow-sm rounded-xl p-4">
       <div className="flex flex-col gap-2 h-full">
         <div className="flex items-center gap-2 pb-4 border-b border-secondary-300">
           <Image src={icon} width={24} height={24} alt="icon" />
           <h3>{title}</h3>
-          <Link
-            href={pageUrl}
-            className="ml-auto text-xs text-primary hover:bg-primary/10 transition-all p-2 border border-secondary-300 rounded-[8px] shadow hover:shadow-none"
-          >
-            {pageTitle}
-          </Link>
+          <LinkCard url={pageUrl} title={pageTitle}/>
         </div>
         <div className="flex-grow">{children}</div>
       </div>
---
+++ app/(dashboard)/dashboard/components/LinkCard.tsx
@@ -0,0 +1,16 @@
+import React, { PropsWithChildren } from "react";
+import Link from "next/link";
+
+export const LinkCard = (
+  props: PropsWithChildren<{ url: string; title: string }>
+) => {
+  const { url, title } = props;
+  return (
+    <Link
+      href={url}
+      className="ml-auto text-xs text-primary hover:bg-primary/10 transition-all p-2 border border-secondary-300 rounded-[8px] shadow hover:shadow-none"
+    >
+      {title}
+    </Link>
+  );
+};

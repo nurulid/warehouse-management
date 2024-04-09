b253e19df4efe2f7681673db40b39bbc9f0f87c3
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-09 11:00:57 +0700

    tweaks layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-dvh overflow-hidden">
       <Sidebar />
-      <section className="flex-grow bg-[#FBFBFB]">{children}</section>
+      <section className="bg-[#FBFBFB] w-full lg:w-[calc(100%-240px)]">{children}</section>
     </main>
   );
 }

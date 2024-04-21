df482c8c75011091fc267968a58bbea807f9ae59
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-21 14:19:31 +0700

    tweaks sidebar

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,23 +1,28 @@
-import React from "react";
-import Image from "next/image";
-import { Search } from "./components/Search";
-import { CardGrid } from "./components/CardGrid";
-import { CardTrack } from "./components/CardTrack";
-import { TableOrders } from "./components/TableOrders";
-import { ShipmentsCard } from "./components/ShipmentsCard";
-import { UsageCard } from "./components/UsageCard";
+import React from 'react';
+import Image from 'next/image';
+import { Search } from './components/Search';
+import { CardGrid } from './components/CardGrid';
+import { CardTrack } from './components/CardTrack';
+import { TableOrders } from './components/TableOrders';
+import { ShipmentsCard } from './components/ShipmentsCard';
+import { UsageCard } from './components/UsageCard';
 
 export default function Page() {
   return (
     <>
       <div className="p-4 border-b border-secondary-200 mr-0 lg:mr-6">
-        <Search maxWidth="558px"/>
+        <Search maxWidth="558px" />
       </div>
       <div className="h-full overflow-scroll">
         <div className="py-4 px-4 sm:px-6 mb-4 h-full">
           <div className="mb-4 inline-flex gap-2 items-center">
             <figure className="bg-white border border-secondary-200 rounded p-2">
-              <Image src="/icons/building-2-line.svg" height={24} width={24} alt="icon"/>
+              <Image
+                src="/icons/building-2-line.svg"
+                height={24}
+                width={24}
+                alt="icon"
+              />
             </figure>
             <div>
               <h2 className="text-xl font-[500] leading-6">Hello, Ernst</h2>
@@ -27,14 +32,16 @@ export default function Page() {
           <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mb-7">
             <UsageCard />
             <ShipmentsCard />
-            <CardGrid 
+            <CardGrid
               title="Vehicle Tracking"
               icon="/icons/focus-3-line.svg"
               pageUrl="/vehicles"
               pageTitle="View Tracking"
-            > 
+            >
               <div>
-                <h3 className="uppercase text-ink-100 text-xs mb-[14px]">Vehicles in transit</h3>
+                <h3 className="uppercase text-ink-100 text-xs mb-[14px]">
+                  Vehicles in transit
+                </h3>
                 <CardTrack />
                 <CardTrack />
               </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import { PropsWithChildren } from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { PropsWithChildren, createContext, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 import {
   DashboardIcon,
   BoxIcon,
@@ -12,12 +13,50 @@ import {
   TeamIcon,
   QuestionIcon,
   LogoutIcon,
-} from "@/components/Icons";
-import Image from "next/image";
-import { UserMenu } from "./UserMenu";
+} from '@/components/Icons';
+import { UserMenu } from './UserMenu';
+
+interface CurrentUserContextType {
+  username: string;
+}
+
+const CurrentUserContext = createContext<CurrentUserContextType | null>(null);
+
+const sidebarMenu = [
+  {
+    name: 'Dashboard',
+    link: '/dashboard',
+    icon: DashboardIcon,
+  },
+  {
+    name: 'Products',
+    link: '/products',
+    icon: BoxIcon,
+  },
+  {
+    name: 'Orders',
+    link: '/',
+    icon: FileIcon,
+  },
+  {
+    name: 'Shipments',
+    link: '/',
+    icon: TimeZoneIcon,
+  },
+  {
+    name: 'Vehicles',
+    link: '/',
+    icon: TruckIcon,
+  },
+  {
+    name: 'Customers',
+    link: '/',
+    icon: TeamIcon,
+  },
+];
 
 const SidebarLink = (
-  props: PropsWithChildren<{ href: string; isDanger?: boolean; }>
+  props: PropsWithChildren<{ href: string; isDanger?: boolean }>
 ) => {
   const { href, children, isDanger = false } = props;
   const currentPath = usePathname();
@@ -26,18 +65,20 @@ const SidebarLink = (
     <li
       style={
         {
-          "--fill":
-            (!active && !isDanger && "#908C95") ||
-            (active && "#6E3FF3") ||
-            (isDanger && "#F31B09"),
+          '--fill':
+            (!active && !isDanger && '#908C95') ||
+            (active && '#6E3FF3') ||
+            (isDanger && '#F31B09'),
         } as any
       }
       className={[
-        "py-2 px-4 rounded-[8px] transition-all",
-        (!active && !isDanger && "text-secondary-500 hover:bg-secondary-200/40") ||
-        (active && "text-primary bg-primary/10") ||
-        (isDanger && "text-danger hover:bg-danger/10"),
-      ].join(" ")}
+        'py-2 px-4 rounded-[8px] transition-all',
+        (!active &&
+          !isDanger &&
+          'text-secondary-500 hover:bg-secondary-200/40') ||
+          (active && 'text-primary bg-primary/10') ||
+          (isDanger && 'text-danger hover:bg-danger/10'),
+      ].join(' ')}
     >
       <Link href={href} className="flex items-center gap-2 text-sm">
         {children}
@@ -47,58 +88,46 @@ const SidebarLink = (
 };
 
 export const Sidebar = () => {
+  const [currentUser, setCurrentUser] = useState<CurrentUserContextType>({
+    username: "filiptammergard",
+  });
+
   return (
-    <section className="sidebar hidden lg:block w-[240px] bg-secondary-75">
-      <div className="flex flex-col h-full px-6 py-8">
-        <div className="mb-8 font-bold text-2xl">
-          <Image src={"/brand-logo.svg"} width={32} height={32} alt="Logo" />
-        </div>
-        <nav className="sidebar__nav">
-          <ul className="space-y-2">
-            <SidebarLink href="/dashboard">
-              <DashboardIcon />
-              Dashboard
-            </SidebarLink>
-            <SidebarLink href="/products">
-              <BoxIcon />
-              Products
-            </SidebarLink>
-            <SidebarLink href="#">
-              <FileIcon />
-              Orders
-            </SidebarLink>
-            <SidebarLink href="#">
-              <TimeZoneIcon />
-              Shipments
-            </SidebarLink>
-            <SidebarLink href="#">
-              <TruckIcon />
-              Vehicles
-            </SidebarLink>
-            <SidebarLink href="#">
-              <TeamIcon />
-              Customers
-            </SidebarLink>
-          </ul>
-        </nav>
-        <div className="mt-auto">
-          <div className="dropdown mb-2">
-            <UserMenu />
+    <CurrentUserContext.Provider value={currentUser}>
+      <section className="sidebar hidden lg:block w-[240px] bg-secondary-75">
+        <div className="flex flex-col h-full px-6 py-8">
+          <div className="mb-8 font-bold text-2xl">
+            <Image src={'/brand-logo.svg'} width={32} height={32} alt="Logo" />
           </div>
           <nav className="sidebar__nav">
-            <ul>
-              <SidebarLink href="#">
-                <QuestionIcon />
-                Help & Support
-              </SidebarLink>
-              <SidebarLink isDanger href="#">
-                <LogoutIcon />
-                Sign Out
-              </SidebarLink>
+            <ul className="space-y-2">
+              {sidebarMenu.map((item, i) => (
+                <SidebarLink key={i} href={item.link}>
+                  <item.icon />
+                  {item.name}
+                </SidebarLink>
+              ))}
             </ul>
           </nav>
+          <div className="mt-auto">
+            <div className="dropdown mb-2">
+              <UserMenu />
+            </div>
+            <nav className="sidebar__nav">
+              <ul>
+                <SidebarLink href="#">
+                  <QuestionIcon />
+                  Help & Support
+                </SidebarLink>
+                <SidebarLink isDanger href="#">
+                  <LogoutIcon />
+                  Sign Out
+                </SidebarLink>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </CurrentUserContext.Provider>
   );
 };
